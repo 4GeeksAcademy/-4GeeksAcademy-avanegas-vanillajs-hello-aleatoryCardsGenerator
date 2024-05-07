728d07c3984d992c75e17a265417d85f17dd7a70
Angie Vanegas P <arco527@gmail.com>
2024-05-07 22:08:38 +0000

    Add colors at the symbols and functionality

--- src/app.js
+++ src/app.js
@@ -7,40 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const carddee = () => {};
-  function cardValue() {
-    let cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
-    let randomCards = Math.floor(Math.random() * 14);
-    let selectCard = cards[randomCards];
+  const cardValue = () => {
+    const cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
+    const randomCards = Math.floor(Math.random() * 14);
+    const selectCard = cards[randomCards];
 
-    console.log(selectCard);
+    // console.log(selectCard);
 
-    let paragraph = document.createElement("p");
+    const paragraph = document.createElement("p");
     paragraph.textContent = selectCard;
 
-    let container = document.querySelector(".card-number-center");
+    const container = document.querySelector(".card-number-center");
     container.appendChild(paragraph);
-  }
+  };
+
   cardValue();
 
-  function cardsSymbols() {
-    let symbols = ["♦", "♥", "♠", "♣"];
-    let symbolsRandom = Math.floor(Math.random() * 4);
-    let selectSymbols = symbols[symbolsRandom];
+  const cardsSymbols = () => {
+    const symbols = ["♦", "♥", "♠", "♣"];
+    const symbolsRandom = Math.floor(Math.random() * 4);
+    const selectSymbols = symbols[symbolsRandom];
 
-    console.log(selectSymbols);
+    // console.log(selectSymbols);
 
-    let paragraphSymbolStart = document.createElement("p");
+    const paragraphSymbolStart = document.createElement("p");
     paragraphSymbolStart.textContent = selectSymbols;
 
-    let containerSymbolStart = document.querySelector(".card-symbol-start");
+    const containerSymbolStart = document.querySelector(".card-symbol-start");
     containerSymbolStart.appendChild(paragraphSymbolStart);
 
-    let paragraphSymbolEnd = document.createElement("p");
+    const paragraphSymbolEnd = document.createElement("p");
     paragraphSymbolEnd.textContent = selectSymbols;
 
-    let containerSymbolEnd = document.querySelector(".card-symbol-end");
+    const containerSymbolEnd = document.querySelector(".card-symbol-end");
     containerSymbolEnd.appendChild(paragraphSymbolEnd);
-  }
+
+    if (selectSymbols === "♦" || selectSymbols === "♥") {
+      containerSymbolStart.appendChild(paragraphSymbolStart).style.color =
+        "red";
+      containerSymbolEnd.appendChild(paragraphSymbolEnd).style.color = "red";
+    } else {
+      containerSymbolStart.appendChild(paragraphSymbolStart).style.color =
+        "black";
+      containerSymbolEnd.appendChild(paragraphSymbolEnd).style.color = "black";
+    }
+
+    containerSymbolStart.appendChild(paragraphSymbolStart);
+    containerSymbolEnd.appendChild(paragraphSymbolEnd);
+  };
   cardsSymbols();
 };
