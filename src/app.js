bdef52e0b3b25af38ed9216b55ab8c56d83a9d43
Angie Vanegas P <arco527@gmail.com>
2024-05-07 20:53:13 +0000

    Add functionality done

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-
+  const carddee = () => {};
   function cardValue() {
     let cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
     let randomCards = Math.floor(Math.random() * 14);
@@ -22,4 +22,25 @@ window.onload = function() {
     container.appendChild(paragraph);
   }
   cardValue();
+
+  function cardsSymbols() {
+    let symbols = ["♦", "♥", "♠", "♣"];
+    let symbolsRandom = Math.floor(Math.random() * 4);
+    let selectSymbols = symbols[symbolsRandom];
+
+    console.log(selectSymbols);
+
+    let paragraphSymbolStart = document.createElement("p");
+    paragraphSymbolStart.textContent = selectSymbols;
+
+    let containerSymbolStart = document.querySelector(".card-symbol-start");
+    containerSymbolStart.appendChild(paragraphSymbolStart);
+
+    let paragraphSymbolEnd = document.createElement("p");
+    paragraphSymbolEnd.textContent = selectSymbols;
+
+    let containerSymbolEnd = document.querySelector(".card-symbol-end");
+    containerSymbolEnd.appendChild(paragraphSymbolEnd);
+  }
+  cardsSymbols();
 };
