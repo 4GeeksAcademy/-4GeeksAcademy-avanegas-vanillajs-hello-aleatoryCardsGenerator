8b391fe3bf6ff365e1644e0d7510fd4b4e3fb5f6
Angie Vanegas P <arco527@gmail.com>
2024-05-07 20:20:17 +0000

    Add cardValue function and works

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function cardValue() {
+    let cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
+    let randomCards = Math.floor(Math.random() * 14);
+    let selectCard = cards[randomCards];
+
+    console.log(selectCard);
+
+    let paragraph = document.createElement("p");
+    paragraph.textContent = selectCard;
+
+    let container = document.querySelector(".card-number-center");
+    container.appendChild(paragraph);
+  }
+  cardValue();
 };
