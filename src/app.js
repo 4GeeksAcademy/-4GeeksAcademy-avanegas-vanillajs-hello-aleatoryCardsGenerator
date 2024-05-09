80f90ac87b5b6f6b1d295751b832cf5b25992806
Angie Vanegas P <arco527@gmail.com>
2024-05-09 16:07:09 +0000

    Refact js file

--- src/app.js
+++ src/app.js
@@ -5,78 +5,78 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-	//write your code here
-	const cardValue = () => {
-		const cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
-		const randomCards = Math.floor(Math.random() * 14);
-		const selectCard = cards[randomCards];
+window.onload = function() {
+  //write your code here
+  const cardValue = () => {
+    const cards = ["As", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
+    const randomCards = Math.floor(Math.random() * 14);
+    const selectCard = cards[randomCards];
 
-		const paragraph = document.createElement("p");
-		paragraph.textContent = selectCard;
+    const paragraph = document.createElement("p");
+    paragraph.textContent = selectCard;
 
-		const container = document.querySelector(".card-number-center");
-		container.appendChild(paragraph);
-	};
+    const container = document.querySelector(".card-number-center");
+    container.appendChild(paragraph);
+  };
 
-	cardValue();
+  cardValue();
 
-	const cardsSymbols = () => {
-		const symbols = ["♦", "♥", "♠", "♣"];
-		const symbolsRandom = Math.floor(Math.random() * 4);
-		const selectSymbols = symbols[symbolsRandom];
+  const cardsSymbols = () => {
+    const symbols = ["♦", "♥", "♠", "♣"];
+    const symbolsRandom = Math.floor(Math.random() * 4);
+    const selectSymbols = symbols[symbolsRandom];
 
-		const paragraphSymbolStart = document.createElement("p");
-		paragraphSymbolStart.textContent = selectSymbols;
+    const paragraphSymbolStart = document.createElement("p");
+    paragraphSymbolStart.textContent = selectSymbols;
 
-		const paragraphSymbolEnd = document.createElement("p");
-		paragraphSymbolEnd.textContent = selectSymbols;
+    const paragraphSymbolEnd = document.createElement("p");
+    paragraphSymbolEnd.textContent = selectSymbols;
 
-		if (selectSymbols === "♦" || selectSymbols === "♥") {
-			paragraphSymbolStart.style.color = "red";
-			paragraphSymbolEnd.style.color = "red";
-		} else {
-			paragraphSymbolStart.style.color = "black";
-			paragraphSymbolEnd.style.color = "black";
-		}
-		paragraphSymbolEnd.style.transform = "rotate(180deg)";
+    if (selectSymbols === "♦" || selectSymbols === "♥") {
+      paragraphSymbolStart.style.color = "red";
+      paragraphSymbolEnd.style.color = "red";
+    } else {
+      paragraphSymbolStart.style.color = "black";
+      paragraphSymbolEnd.style.color = "black";
+    }
+    paragraphSymbolEnd.style.transform = "rotate(180deg)";
 
-		const containerSymbolStart = document.querySelector(".card-symbol-start");
-		containerSymbolStart.appendChild(paragraphSymbolStart);
+    const containerSymbolStart = document.querySelector(".card-symbol-start");
+    containerSymbolStart.appendChild(paragraphSymbolStart);
 
-		const containerSymbolEnd = document.querySelector(".card-symbol-end");
-		containerSymbolEnd.appendChild(paragraphSymbolEnd);
-	};
-	cardsSymbols();
+    const containerSymbolEnd = document.querySelector(".card-symbol-end");
+    containerSymbolEnd.appendChild(paragraphSymbolEnd);
+  };
+  cardsSymbols();
 
-	// New card
-	const buttonMakeNewCard = document.querySelector(".buttonNewCard");
-	console.log("buttonID");
-	buttonMakeNewCard.addEventListener("click", event => {
-		console.log("clickNewCard");
-		window.location.reload();
-	});
+  // New card
+  const buttonMakeNewCard = document.querySelector(".buttonNewCard");
+  console.log("buttonID");
+  buttonMakeNewCard.addEventListener("click", event => {
+    console.log("clickNewCard");
+    window.location.reload();
+  });
 
-	const timerReloadButton = document.querySelector(".buttonTimer");
+  const timerReloadButton = document.querySelector(".buttonTimer");
 
-	timerReloadButton.addEventListener("click", event => {
-		setTimeout(() => {
-			window.location.reload();
-		}, 5000);
-	});
+  timerReloadButton.addEventListener("click", event => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  });
 
-	//Resize
-	const buttonResize = document.querySelector(".buttonClickResize");
+  //Resize
+  const buttonResize = document.querySelector(".buttonClickResize");
 
-	buttonResize.addEventListener("click", event => {
-		const stringHeight = document.querySelector(".heightSize").value;
-		const stringWidth = document.querySelector(".widthSize").value;
-		const resizeCard = document.querySelector(".card");
-		console.log("variables");
-		if (resizeCard && resizeCard.style) {
-			resizeCard.style.height = stringHeight + "px";
-			resizeCard.style.width = stringWidth + "px";
-			console.log("Works");
-		}
-	});
+  buttonResize.addEventListener("click", event => {
+    const stringHeight = document.querySelector(".heightSize").value;
+    const stringWidth = document.querySelector(".widthSize").value;
+    const resizeCard = document.querySelector(".card");
+    console.log("variables");
+    if (resizeCard && resizeCard.style) {
+      resizeCard.style.height = stringHeight + "px";
+      resizeCard.style.width = stringWidth + "px";
+      console.log("Works");
+    }
+  });
 };
